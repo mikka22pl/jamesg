944a7226d2cc407c4d84b3118b57e601b5a04653
Mikka <folke@wp.pl>
2019-05-01 12:54:17 +0200

    Client layout

--- src/main/java/pl/ulv/jamesg/Client.java
+++ src/main/java/pl/ulv/jamesg/Client.java
@@ -1,10 +1,14 @@
 package pl.ulv.jamesg;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -15,10 +19,33 @@ public class Client extends JFrame {
 	private ObjectOutputStream output;
 	private ObjectInputStream input;
 	private String message = "";
-	private String ServerIP;
+	private String serverIP;
 	private Socket connection;
 	
-	public Client() {
+	public Client(String host) {
+		super("Client");
+		setUpLayout(host);
+	}
+	
+	private void setUpLayout(String host) {
+		serverIP = host;
+		userText = new JTextField();
+		userText.setEditable(false);
+		userText.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sendData(e.getActionCommand());
+				userText.setText("");
+			}
+		});
+		add(userText, BorderLayout.NORTH);
+		chatWindow = new JTextArea();
+		add(new JScrollPane(chatWindow));
+		setSize(300, 150);
+		setVisible(true);
+	}
+	
+	private void sendData(String data) {
 		
 	}
 }
