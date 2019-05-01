3689b9eb444799806f89d16c4e614046c396597f
Mikka <folke@wp.pl>
2019-05-01 20:54:17 +0200

    Client - working

--- src/main/java/pl/ulv/jamesg/Client.java
+++ src/main/java/pl/ulv/jamesg/Client.java
@@ -3,14 +3,18 @@ package pl.ulv.jamesg;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.EOFException;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 public class Client extends JFrame {
 	
@@ -25,6 +29,61 @@ public class Client extends JFrame {
 	public Client(String host) {
 		super("Client");
 		setUpLayout(host);
+	}
+	
+	public void startRunning() {
+		try {
+			connectToServer();
+			setUpStreams();
+			whileChatting();
+		} catch (EOFException e) {
+			showMessage("\n Client terminated the connection");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection();
+		}
+	}
+	
+	private void connectToServer() throws IOException {
+		showMessage("Attempting connection...\n");
+		connection = new Socket(InetAddress.getByName(serverIP), 6789);
+		showMessage("Connection Established! Connection to: " + connection.getInetAddress().getHostName());
+	}
+	
+	private void closeConnection() {
+		showMessage("\n Closing the connection");
+		ableToType(false);
+		try {
+			output.close();
+			input.close();
+			connection.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void setUpStreams() throws IOException {
+		output = new ObjectOutputStream(connection.getOutputStream());
+		output.flush();
+		input = new ObjectInputStream(connection.getInputStream());
+		showMessage("\n The streams are now set up! \n");
+	}
+	
+	private void whileChatting() throws IOException {
+		ableToType(true);
+		do {
+			try {
+				message = (String) input.readObject();
+				showMessage("n" + message);
+			} catch (ClassNotFoundException e) {
+				showMessage("Unknown data received!");
+			}
+		} while (!message.equals("SERVER - END"));
+	}
+	
+	private void showMessage(String message) {
+		
 	}
 	
 	private void setUpLayout(String host) {
@@ -46,6 +105,20 @@ public class Client extends JFrame {
 	}
 	
 	private void sendData(String data) {
-		
+		try {
+			output.writeObject("CLIENT - " + data);
+			output.flush();
+		} catch (IOException e) {
+			chatWindow.append("\n Oops! Something went wrong!");
+		}
+	}
+	
+	private void ableToType(final boolean status) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				userText.setEditable(status);
+			}
+		});
 	}
 }
